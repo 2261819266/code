591af7b674d705f0cd44a76ef0834d316571df11
Yu <2261819266@qq.com>
2022-07-07 09:45:22 +0800

    master

--- luogu/P5686.cpp
+++ luogu/P5686.cpp
@@ -1,12 +1,13 @@
 #include <cstdio>
-#define ll long long
+#define ll __int128_t
 
 const int maxn = 5e5 + 8, M = 1e9 + 7;
 ll a[2][maxn];
 ll b[2][maxn];
+ll c[2][maxn];
 
 ll get(int k, int l, int r) {
-    return (b[k][r] - b[k][l - 1]) % M;
+    return (b[k][r] - b[k][l - 1]);
 }
 
 int main() {
@@ -19,14 +20,17 @@ int main() {
     for (int i = 0; i < 2; i++) {
         for (int j = 1; j <= n; j++) {
             scanf("%d", &a[i][j]);
-            b[i][j] = b[i][j - 1] + a[i][j];
+            b[i][j] = (b[i][j - 1] + a[i][j]) % M;
+            c[i][j] = (c[i][j - 1] + b[i][j]) % M;
         }
     }
     for (int i = 1; i <= n; i++) {
-        for (int j = i; j <= n; j++) {
-            ans += get(0, i, j) * get(1, i, j);
-            ans %= M;
-        }
+        ans += b[0][i] * b[1][i];
+        // ans %= M;
     }
-    printf("%lld", ans);
+    ans *= n;
+    for (int i = 1; i <= n; i++) {
+        ans -= b[0][i] * (c[1][n] - b[1][i]);
+    }
+    printf("%lld", (long long)(ans % M));
 }
--- luogu/P7911.cpp
+++ luogu/P7911.cpp
@@ -1,37 +1,51 @@
 #include <cstdio>
 #include <cstring>
+#define Is1(x) (x >= 0 && x < 256)
+#define Is2(x) (x >= 0 && x < 65536)
 
 const int maxn = 1e3 + 2;
-struct Node {
-#define isNumber(x) (x >= '0' && x <= '9')
-#define isString(x) (x == '.' || x == ':')
-    int k;
-    char a[100];
-    void scan() {
-        char s[10];
-        scanf("%s", s);
-        if (*s == 'C') k = 1;
-        scanf("%s", a);
-    }
 
-    operator bool() {
-        if (a[0] == '0') return false;
-        for (int i = 0, j = 0; a[i]; i++) {
-            if (a[i] == ':' && j <= 2 || a[i] == '.' && j > 2) return false;
-            if (isString(a[i])) j++;
-        }
-
-    }
+struct Server {
+	int a, b, c, d, e;
+	Server() { a = b = c = d = e = -1; }
+	bool operator==(const Server &y) {
+		return a == y.a && b == y.b && c == y.c && d == y.d && e == y.e;
+	}
 } a[maxn];
+int cnt;
+
+int cmp(const char *s, bool IsServer) {
+	Server x;
+	cnt++;
+	int t = sscanf(s, "%d.%d.%d.%d:%d", &x.a, &x.b, &x.c, &x.d, &x.e);
+	if (t - 5) return -1;
+	if (!(Is1(x.a) && Is1(x.b) && Is1(x.c) && Is1(x.d) && Is2(x.e))) return -1;
+	char s2[100];
+	sprintf(s2, "%d.%d.%d.%d:%d", x.a, x.b, x.c, x.d, x.e);
+	if (strcmp(s, s2)) return -1;
+	for (int i = 0; i < cnt; i++) {
+		if (x == a[i]) return i + 1;
+	}
+	if (IsServer) a[cnt - 1] = x;
+	return 0;
+}
 
 int main() {
 #ifdef LOCAL
-    LOCALfo
+	LOCALfo
 #endif
-    int n;
-    scanf("%d", &n);
-    for (int i = 0; i < n; i++) {
-        a[i].scan();
-        printf("%d", a[i].operator bool());
-    }
+	int n;
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		char s[100];
+		scanf("%s", s);
+		int k = (*s == 'S');
+		scanf("%s", s);
+		int x = cmp(s, k);
+		if (x < 0) printf("ERR\n");
+		if (k && x == 0) printf("OK\n");
+		if (!k && x == 0) printf("FAIL\n");
+		if (k && x > 0) printf("FAIL\n");
+		if (!k && x > 0) printf("%d\n", x);
+	}
 }

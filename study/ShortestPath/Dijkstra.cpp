abb486d19c3c5df59e7a32292faa37ad5b3fdf9e
Yu <2261819266@qq.com>
2022-07-08 21:58:20 +0800

    master

--- study/ShortestPath/Dijkstra.cpp
+++ study/ShortestPath/Dijkstra.cpp
@@ -3,17 +3,20 @@
 #include <algorithm>
 #include <queue>
 
-#define f(x, y, z) for (int x = y, __ = z; x < z; ++x)
+#define f(x, y, z) for (int x = y, __ = z; x < __; ++x)
 
-const int maxn = 1e5 + 8, maxm = 2e5 + 8;
-std::priority_queue<int, std::vector<int>, std::greater<int>> q;
+const int maxn = 1e5 + 8, maxm = 5e5 + 8;
 int dis[maxn], head[maxn], cnt, vis[maxn];
 struct E {
     int to, w, next;
-} edge[maxn << 2];
+} edge[maxm << 1];
+struct Node {
+    int u, dis;
+    bool operator<(const Node &y) const { return dis > y.dis; }
+};
+std::priority_queue<Node> q;
 
 void addEdge(int u, int v, int w) {
-    if (!cnt) memset(head, -1, sizeof(head));
     edge[cnt] = {v, w, head[u]};
     head[u] = cnt++;
 }
@@ -21,16 +24,16 @@ void addEdge(int u, int v, int w) {
 void dijkstra(int n, int s) {
     f(i, 1, n + 1) dis[i] = 0x7fffffff;
     dis[s] = 0;
-    q.push(s);
-    f(i, 0, n) {
-        int u = q.top();
+    q.push({s, 0});
+    while (!q.empty()) {
+        int u = q.top().u;
         q.pop();
         if (vis[u]) continue;
         vis[u] = 1;
         for (int i = head[u]; ~i; i = edge[i].next) {
             int to = edge[i].to;
             dis[to] = std::min(dis[to], dis[u] + edge[i].w);
-            q.push(to);
+            q.push({to, dis[to]});
         }
     }
 }
@@ -39,6 +42,7 @@ int main() {
 #ifdef LOCAL
     LOCALfo
 #endif
+    memset(head, -1, sizeof(head));
     int n, m, s;
     scanf("%d%d%d", &n, &m, &s);
     f(i, 0, m) {

279919442c8a67559b51e04cc265bdf58e50dff3
Yu <2261819266@qq.com>
2022-07-11 10:19:29 +0800

    master

--- luogu/P1880.cpp
+++ luogu/P1880.cpp
@@ -1,11 +1,15 @@
-// #include <cstdio>
-#include <cmath>
+#include <cstdio>
+#include <algorithm>
+#define f(x, y, z) for (auto x = y, __ = z; x < __; ++x)
 
-const int maxn = 500;
+const int maxn = 105;
+int n, a[maxn], dp[maxn][maxn];
 
-struct node {
-	int data;
-	node *next, *last;
-} head;
-std::__are_same<int,int>
-
+int main() {
+#ifdef LOCAL
+	LOCALfo
+#endif
+	scanf("%d", &n);
+	f(i, 1, n + 1) scanf("%d", a + i);
+	
+}
--- luogu/P7925.cpp
+++ luogu/P7925.cpp
@@ -1,19 +1,30 @@
 #include <cstdio>
 #include <vector>
+#include <algorithm>
+#define ll long long
 #define f(x, y, z) for (auto x = y, __ = z; x < __; ++x)
 
 const int maxn = 6005;
-std::vector<int> e[maxn];
-int a[maxn], s, n;
+std::vector<ll> e[maxn];
+ll a[maxn], s;
+int n;
+struct Node {
+    ll v, need;
+} dp[maxn];
+
+
+void dfs(int r) {
+    if (e[r].empty()) 
+}
 
 int main() {
 #ifdef LOCAL
     LOCALfo
 #endif
-    scanf("%d%d", &n, &s);
+    scanf("%d%lld", &n, &s);
     f(i, 1, n) {
-        int x;
-        scanf("%d", &x);
+        ll x;
+        scanf("%lld", &x);
         e[x].push_back(i + 1);
     }
 }

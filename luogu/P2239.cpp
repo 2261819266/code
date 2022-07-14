c2f5c22bd6246f8e2f944c598ef7524d5be27c7d
Yu <2261819266@qq.com>
2022-07-15 07:34:00 +0800

    master

--- luogu/P2239.cpp
+++ luogu/P2239.cpp
@@ -15,20 +15,23 @@ int main() {
     LOCALfo
 #endif
     scanf("%d%d%d", &n, &x, &y);
-    int now = 1, nx = 1, ny = 1, nn = n - 1;
-    f(i, 0, getDepth(x, y)) {
-        now += 4 * (n - 2 * i - 1);
-        nx++, ny++, nn -= 2;
-    }
+    int now = 1, nx = 1, ny = 1, nn = n - 1, d = getDepth(x, y);
+    // f(i, 0, d) {
+    //     now += 4 * (n - 2 * i - 1);
+    //     nx++, ny++, nn -= 2;
+    // }
+    now += 4 * (nn + n - d * 2 + 1) * d / 2;
+    nx = ny = d + 1;
+    nn = n - d * 2 - 1;
     if (nx != x) {
         now += nn;
-        nx += nn;
+        ny += nn;
         if (ny != y) {
             now += nn;
-            ny += nn;
+            nx += nn;
             if (nx != x) {
                 now += nn;
-                nx -= nn;
+                ny -= nn;
             }
         }
     }

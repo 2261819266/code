b4fef59b3ebff0441966fc10795c529331210c77
yuxinhao <2261819266@qq.com>
2023-09-22 11:57:23 +0800

    master

--- luogu/P1052.cpp
+++ luogu/P1052.cpp
@@ -1,7 +1,35 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+
+using std::cin;
+using std::vector;
+using std::min;
+using std::max;
+using std::cout;
+using std::endl;
+
+
+vector<int> a, f;
 
 void P1052() {
-
+    int l, s, t, m;
+    cin >> l >> s >> t >> m;
+    a.assign(l + 1, 0);
+    f.assign(l + 1, m);
+    while (m--) {
+        int x;
+        cin >> x;
+        a[x] = 1;
+    }
+    f[0] = 0;
+    for (int i = s; i <= l; i++) {
+        int L = max(0, i - t);
+        for (int j = L; j <= i - s; j++) {
+            f[i] = min(f[i], f[j]) + a[i];
+        }
+    }
+    cout << f[l] << endl;
 }
 
 int main() {

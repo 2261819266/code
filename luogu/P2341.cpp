2041308045470a7c1673f2d881f76e9bbc93d167
yuxinhao <2261819266@qq.com>
2023-09-24 17:31:34 +0800

    master

--- luogu/P2341.cpp
+++ luogu/P2341.cpp
@@ -1,74 +1,50 @@
-#include <cstdio>
 #include <algorithm>
+#include <iostream>
 #include <vector>
-#include <cstring>
-#include <stack>
 
-const int maxn = 5e4 + 8;
-std::vector<int> e[maxn];
-int vis[maxn], dfn[maxn], low[maxn], cnt, a[maxn], col[maxn], num, size[maxn];
-std::stack<int> s;
+using std::cin;
+using std::cout;
+using std::vector;
+using std::min;
 
-void dfs(int u) {
-    vis[u] = true;
+const int maxn = 1e4 + 8;
+vector<int> e[maxn], dfn, low, in;
+int cnt = 0;
+
+int tarjan(int u, int n) {
+    if (low[u]) {
+        if (!in[u]) return n;
+        return low[u];
+    }
+    in[u] = true;
     dfn[u] = low[u] = ++cnt;
-    s.push(u);
     for (int v : e[u]) {
-        if (!dfn[v]) {
-            dfs(v);
-            low[u] = std::min(low[u], low[v]);
-        } else if (vis[v]) {
-            low[u] = std::min(low[u], low[v]);
-        }
+        low[u] = min(low[u], tarjan(v, n));
     }
-    if (dfn[u] == low[u]) {
-        num++;
-        while (s.top() != u) {
-            col[s.top()] = num;
-            size[num]++;
-            vis[s.top()] = false;
-            s.pop();
-        }
-        col[s.top()] = num;
-        size[num]++;
-        vis[s.top()] = false;
-        s.pop();
-    }
+    in[u] = false;
+    return low[u];
 }
 
-
-int main() {
+void P2341() {
     int n, m;
-    scanf("%d%d", &n, &m);
+    cin >> n >> m;
     for (int i = 0; i < m; i++) {
         int u, v;
-        scanf("%d%d", &u, &v);
+        cin >> u >> v;
         e[u].push_back(v);
     }
+    low.assign(n + 1, 0);
+    dfn.assign(n + 1, 0);
+    in.assign(n + 1, 0);
     for (int i = 1; i <= n; i++) {
-        if (!dfn[i]) dfs(i);
-        // printf("%d ", col[i]);
+        tarjan(i, n);
+        // cout << low[i] << " ";
     }
-    for (int u = 1; u <= n; u++) {
-        for (int v : e[u]) {
-            if (col[u] != col[v]) {
-                a[col[u]]++;
-            }
-        }
-    }
-    int x = 0;
-    for (int i = 1; i <= n; i++) {
-        if (!a[col[i]]) {
-            if (!x) x = col[i];
-            else if (x != col[i]) {
-                puts("0");
-                return 0;
-            }
-        }
-    }
-    int ans = 0;
-    for (int i = 1; i <= n; i++) {
-        if (col[i] == x) ans++;
-    }
-    printf("%d", ans);
+    vector<vector<int>> a;
+    
+}
+
+int main() {
+    P2341();
+    return 0;
 }
